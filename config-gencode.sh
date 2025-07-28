d9127a693286b9895558130c3e338b03d78241af
truongthanh <truongthanh2a3@gmail.com>
2025-07-28 09:26:34 +0700

    update config-gencode.sh - update echo comment

--- config-gencode.sh
+++ config-gencode.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Pull commit"
+echo "🚀Checkout and Pull commit"
 cd /home/max/gencode/docker_gencode/source/VMS_GENCODE
 git checkout master
 git pull origin master
 
 cd /home/max/gencode/docker_gencode
-echo "🧼 Removing old container (if exists)..."
+echo "🚀 Removing old container (if exists)..."
 docker compose rm -sf nginx-php-fpm-8.2
 
-echo "🚧 Rebuilding nginx-php-fpm-8.2 without cache..."
+echo "🚀 Rebuilding nginx-php-fpm-8.2 without cache..."
 docker compose build --no-cache nginx-php-fpm-8.2
 
 echo "🚀 Recreating container..."
@@ -17,7 +17,7 @@ docker compose up -d --no-deps --force-recreate nginx-php-fpm-8.2
 
 echo "✅ Done!"
 
-echo " Running Lavarel setup inside container: nginx-php-fpm-82"
+echo "🚀 Running Lavarel setup inside container: nginx-php-fpm-82"
 
 # docker exec -it nginx-php-fpm-82 bash -c "
 #     cd /var/www/html && \ 
@@ -31,5 +31,5 @@ docker exec -it nginx-php-fpm-82 bash -c "
     npm install && npm run build && \
     composer require laravel/reverb maatwebsite/excel phpoffice/phpspreadsheet:^1.28 --with-all-dependencies
     "
-echo " Lavarel setup completed!"
+echo "🚀 Lavarel setup completed!"
     
